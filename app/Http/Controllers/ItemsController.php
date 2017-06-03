bc16267a288852e879000c563e1092e2c7d3416b
Ivan Kulikov <ikoolik@gmail.com>
2017-06-03 14:30:33 +0300

    возможность удалять товары

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -83,11 +83,12 @@ class ItemsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Item $item
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Item $item)
     {
-        //
+        $item->delete();
+        return redirect('/items');
     }
 }
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -11,12 +11,20 @@
             <th>ID</th>
             <th>Тип проката</th>
             <th>Тип стали</th>
+            <th>Действие</th>
         </tr>
         @foreach($items as $item)
             <tr>
                 <td>{{ $item->id }}</td>
                 <td>{{ $item->rollType->name }}</td>
                 <td>{{ $item->steelType->name }}</td>
+                <td>
+                    <form class="form-inline" action="items/{{ $item->id }}" method="post">
+                        {{ csrf_field() }}
+                        <input type="hidden" name="_method" value="delete">
+                        <button class="btn btn-default"><i class="fa fa-trash"></i></button>
+                    </form>
+                </td>
             </tr>
         @endforeach
     </table>
